2e3c60599bec1f44ad1ce51da2904fbba0800624
angeldavid06 <mendozaangel.3243@gmail.com>
2021-07-09 23:20:34 -0500

    11:20 pm iIplementación de carrusel en examples

--- examples/script.js
+++ examples/script.js
@@ -1,2 +1,12 @@
-const btn_accept = document.getElementById("btn-aceptar")
-const btn_cancel = document.getElementById("btn-cancelar")
+const route_images = [
+    'https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg',
+    'https://ep01.epimg.net/elpais/imagenes/2019/10/30/album/1572424649_614672_1572453030_noticia_normal.jpg',
+    './img/1 (3).jpg',
+    './img/1 (4).jpg',
+    './img/1 (5).jpg',
+    './img/1 (6).jpg'
+];
+
+(() => {
+    render_images(route_images);
+})()
--- scroll_animations/script.js
+++ scroll_animations/script.js
@@ -1,7 +1,6 @@
 let con = document.getElementsByClassName('test');
 
 window.addEventListener('scroll', function(e) {
-    console.log(window.pageYOffset);
     if (window.pageYOffset >= con[0].offsetTop) {
         con[0].classList.add('con-outline');
     } else {
