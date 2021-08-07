84bb566c1a4fae08cbb7b953d3f8b2a4250fd481
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-06 23:25:36 -0500

    2021-08-06 11:25 pm Implementacióo de la función window.print

--- jspdf/script.js
+++ jspdf/script.js
@@ -1,14 +1,6 @@
 if (document.getElementsByClassName('getPDF')) {
     const btn = document.getElementsByClassName('getPDF');
     btn[0].addEventListener('click', () => {
-        // const doc = new jsPDF();
-        const doc = new jsPDF('p', 'mm', 'letter');
-
-        const html = document.getElementsByClassName('container')[0];
-        
-        doc.setFontSize(12);
-
-        doc.fromHTML(html);
-        doc.save('test.pdf');
+        window.print();
     });
 }
