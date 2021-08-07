cff687d65a17947f0afe5c9134cea0d051c37410
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-07 15:15:53 -0500

    2021-08-07 03:15 pm Pruebas de la generación de documentos PDF

---
+++ print/pdf.js
@@ -0,0 +1,5 @@
+(() => {
+    const doc = document.createElement('p');
+    doc.innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum cupiditate cumque iusto inventore earum provident dolorem consequuntur harum, fugit laudantium sequi, sed eius ut exercitationem error alias tenetur nihil dolor?';
+    document.body.appendChild(doc);
+})()
--- print/script.js
+++ print/script.js
@@ -2,5 +2,6 @@ if (document.getElementsByClassName('getPDF')) {
     const btn = document.getElementsByClassName('getPDF');
     btn[0].addEventListener('click', () => {
         printPage('tests.html')      
+        // printPage('../chart/index.html')      
     });
 }
