79b925b4afabe3935aa9f8ba2321bf230f3ff409
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-06 19:57:47 -0500

    2021-08-06 07:57 pm Pruebas de generación de PDF

--- jspdf/script.js
+++ jspdf/script.js
@@ -1,9 +1,14 @@
 if (document.getElementsByClassName('getPDF')) {
     const btn = document.getElementsByClassName('getPDF');
     btn[0].addEventListener('click', () => {
-        var doc = new jsPDF();
+        // const doc = new jsPDF();
+        const doc = new jsPDF('p', 'mm', 'letter');
+
         const html = document.getElementsByClassName('container')[0];
+        
+        doc.setFontSize(12);
+
         doc.fromHTML(html);
-        doc.save('prueba.pdf');
+        doc.save('test.pdf');
     });
 }
