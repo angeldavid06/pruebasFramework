75e78921030b5dc677185efba20ed50a0e1282ed
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-07 14:30:09 -0500

    2021-08-07 02:29 pm Implementación de impresión de reportes desde un documento externo

--- print/script.js
+++ print/script.js
@@ -1,6 +1,6 @@
 if (document.getElementsByClassName('getPDF')) {
     const btn = document.getElementsByClassName('getPDF');
     btn[0].addEventListener('click', () => {
-        window.print();
+        printPage('tests.html')      
     });
 }
